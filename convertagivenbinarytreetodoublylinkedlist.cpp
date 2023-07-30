e9c8f462b69ecbb6dce95660dc93cf8805c477f5
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-07-30 21:07:54 +0530

    Create convertagivenbinarytreetodoublylinkedlist.cpp

---
+++ convertagivenbinarytreetodoublylinkedlist.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+ 
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode 
+    {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+
+void findHead(BinaryTreeNode<int> *root, BinaryTreeNode<int> **head, BinaryTreeNode<int> **prev) {
+    
+   
+    if(root == NULL)
+    {
+        return ;
+    }
+
+    findHead(root->left, head, prev);
+
+    if(*prev == NULL)
+    {
+        *head = root;
+    }
+    else
+    {
+        root->left = *prev;
+        (*prev)->right = root;
+    }
+
+    *prev = root;
+
+    findHead(root->right, head, prev);
+}
+
+BinaryTreeNode<int>* BTtoDLL(BinaryTreeNode<int>* root) {
+    BinaryTreeNode<int>* head = NULL;
+    BinaryTreeNode<int>* prev = NULL;
+    findHead(root, &head, &prev);
+
+    return head;
+}
