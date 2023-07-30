2c828160acf1d40f014d404559ec74a6ffe21d87
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-07-30 20:36:51 +0530

    Create connectnodesatsamelevel.cpp

---
+++ connectnodesatsamelevel.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h> 
+/*
+    ----------------- Binary Tree node class for reference -----------------
+
+    template <typename T>
+    class BinaryTreeNode {
+        public : 
+            T data;
+            BinaryTreeNode<T> *left;
+            BinaryTreeNode<T> *right;
+            BinaryTreeNode<T> *next;
+
+            BinaryTreeNode(T data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+                next = NULL;
+            }
+    };
+*/
+
+void connectNodes(BinaryTreeNode<int> *root) {
+
+   
+    BinaryTreeNode<int> *startNode = root;
+
+    while(startNode != NULL) {
+
+        BinaryTreeNode<int> *ptr = startNode;
+        BinaryTreeNode<int> *previous = NULL;
+        startNode = NULL;
+        
+        
+        while(ptr != NULL) {
+
+            if(ptr->left != NULL) {
+
+                if(previous != NULL) {
+                    previous->next = ptr->left;
+                }
+
+               
+                if(startNode == NULL) {
+                    startNode = ptr->left;
+                }
+
+              
+                previous = ptr->left;
+            }
+
+            if(ptr->right != NULL) {
+
+                if(previous != NULL) {
+                    previous->next = ptr->right;
+                }
+
+               
+                if(startNode == NULL) {
+                    startNode = ptr->right;
+                }
+
+                
+                previous = ptr->right;
+            }
+
+            ptr = ptr->next;
+        }
+    }
+}
