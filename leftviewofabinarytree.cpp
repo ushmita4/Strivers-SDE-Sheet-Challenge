26c9bf3d9ff14fc311d8f7615655b3680365f424
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-07-30 19:44:36 +0530

    Create leftviewofabinarytree.cpp

---
+++ leftviewofabinarytree.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+vector<int> getLeftView(TreeNode<int> *root)
+{
+    
+    
+    vector<int> leftView;
+
+    if (root == NULL)
+    {
+        return leftView;
+    }
+
+   
+    queue<TreeNode<int> *> level;
+
+ 
+    level.push(root);
+    int leftMostNode;
+
+    while (level.empty() == false)
+    {
+        
+      
+        int queueSize = level.size();
+
+       
+        for (int i = 0; i < queueSize; i++)
+        {
+            
+            
+            TreeNode<int> *curr = level.front();
+            level.pop();
+
+           
+            if (i == 0)
+            {
+                leftMostNode = curr->data;
+            }
+
+            
+            if (curr->left != NULL)
+            {
+                level.push(curr->left);
+            }
+
+         
+            if (curr->right != NULL)
+            {
+                level.push(curr->right);
+            }
+        }
+
+        
+        leftView.push_back(leftMostNode);
+    }
+
+    
+    return leftView;
+}
