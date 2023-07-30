9e0168c800b48511cb456b52f1089d741f0c633c
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-07-30 20:48:26 +0530

    Create lcaoftwonodesinabst.cpp

---
+++ lcaoftwonodesinabst.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+/************************************************************
+    Following is the Binary Search Tree node structure
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q)
+{
+    while (root != NULL)
+    {
+        if (root -> data < P -> data && root -> data < Q -> data)
+        {
+            root = root -> right;
+        }
+        else if (root -> data > P -> data && root -> data > Q -> data)
+        {
+            root = root -> left;
+        }
+        else
+        {
+            return root;
+        }
+    }
+}
