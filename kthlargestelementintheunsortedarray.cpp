ead04d593b8b7459e4dd84e830a375a0cd18b8a8
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2023-07-30 21:26:53 +0530

    Create kthlargestelementintheunsortedarray.cpp

---
+++ kthlargestelementintheunsortedarray.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h> 
+
+
+int kthLargest(vector<int>& arr, int size, int K)
+{
+    
+    priority_queue<int, vector<int>, greater<int> >pq;
+    int val;
+    for (int i = 0; i < size; i++)
+    {
+        if (i < K)
+        {
+            pq.push(arr[i]);
+        }
+
+        else
+        {
+            val = pq.top();
+            if (val < arr[i])
+            {
+                pq.pop();
+                pq.push(arr[i]);
+            }
+
+        }
+
+    }
+
+    return pq.top();
+}
